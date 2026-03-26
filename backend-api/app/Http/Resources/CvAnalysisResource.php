2b9fc9b4a39ce7c464315ace58575b7b32dea1e8
YousefAlTohamy <youseftohtoh46@gmail.com>
2026-03-26 22:50:30 +0200

    feat: Adding new fields to CvAnalysisResource

--- backend-api/app/Http/Resources/CvAnalysisResource.php
+++ backend-api/app/Http/Resources/CvAnalysisResource.php
@@ -17,7 +17,11 @@ class CvAnalysisResource extends JsonResource
         return [
             'id'                 => $this->id,
             'parsing_status'     => $this->parsing_status,
-            'completeness_score' => $this->completeness_score,
+            'completeness_score' => (int) $this->completeness_score,
+            'match_score'        => (float) $this->match_score, // حقل جديد
+            'seniority'          => $this->seniority,           // حقل جديد
+            'primary_domain'     => $this->primary_domain,      // حقل جديد
+            'summary'            => $this->summary,             // حقل جديد
             'strengths'          => $this->strengths ?? [],
             'gaps'               => $this->gaps ?? [],
             'red_flags'          => $this->red_flags ?? [],
